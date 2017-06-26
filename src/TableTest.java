1545f2467e220c10e06b010f84d34eef355e08b8
WinChuan <huangchuan5@yonyou.com>
2017-06-26 10:43:50 +0800

    去掉不可编辑前
    Change-Id: Icb4ed684d4dca30a79635853f8e1a4c2a664af91
    Signed-off-by: WinChuan <huangchuan5@yonyou.com>

--- src/TableTest.java
+++ src/TableTest.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableColumn;
 
 
 public class TableTest {
@@ -61,7 +62,7 @@ public class TableTest {
 		String[] columnNames = {"生产订单", "产品","规格","计划数量","完工日期","工段","已排程数量","未排程数量","可用机台","产品生产能力","排程时间","排程数量","2月4号","2月5号"};
 		JTable table = new JTable(cellData, columnNames);
 		panel.add(new JScrollPane(table),BorderLayout.CENTER);
-
+//        table.addColumn(new TableColumn());		
 //		panel.add(table, BorderLayout.CENTER);
 		
 	}
--- src/com/Test.java
+++ src/com/Test.java
@@ -13,24 +13,24 @@ public class Test {
 
 	public static void main(String args[]) {
 		JFrame jf = new JFrame("Cell Combine Table");
-		jf.getContentPane().setLayout(new GridLayout(1, 2, 0, 0));
+		jf.getContentPane().setLayout(new GridLayout(1, 1, 0, 0));
 		JTable cTable = getTable1();
 
 		jf.getContentPane().add(new JScrollPane(cTable));
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setSize(500, 500);
+		jf.setSize(1000, 500);
 		jf.setVisible(true);
 		
-		JTable cTable2 = getTable1();
-
-		jf.getContentPane().add(new JScrollPane(cTable2));
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setSize(1000, 300);
-		jf.setVisible(true);
+//		JTable cTable2 = getTable1();
+//
+//		jf.getContentPane().add(new JScrollPane(cTable2));
+//		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		jf.setSize(1000, 300);
+//		jf.setVisible(true);
 	}
 
 	private static CombineTable getTable1() {
-		String[][] datas = new String[10][6];
+		String[][] datas = new String[9][6];
 		for (int i = 0; i < datas.length; i++) {             //i表示某一行
 			String[] data = datas[i];
 			for (int j = 0; j < data.length; j++) {
@@ -38,29 +38,27 @@ public class Test {
 			}
 			data[0] = String.valueOf((int) (i / 3));         //之后通过data[0]的值对这一列进行合并了
 			data[1] = String.valueOf((int) (i / 3));
-			data[2] = String.valueOf((int) (i / 3));
 		}
 
 		ArrayList<Integer> combineColumns = new ArrayList<Integer>();   //用来保存需要合并的列数
 		combineColumns.add(0);                                          //表示需要合并0列
 		combineColumns.add(1);
-		combineColumns.add(2);
 		CombineData m = new CombineData(datas, combineColumns);
-		String[] columnNames = {"a","b", "c", "d", "e"};       //表头名
+		String[] columnNames = {"生产订单","产品", "可用机台", "产品能力", "排程时间"};       //表头名
 		DefaultTableModel tm = new DefaultTableModel(datas,columnNames);
 		CombineTable cTable = new CombineTable(m, tm);
 
 		TableColumn column = cTable.getColumnModel().getColumn(0);
 		column.setCellRenderer(new CombineColumnRender());
-		column.setWidth(50);
-		column.setMaxWidth(50);
-		column.setMinWidth(50);
+//		column.setWidth(50);                                             //去掉第一列宽度设定
+//		column.setMaxWidth(50);
+//		column.setMinWidth(50);
 		cTable.setCellSelectionEnabled(true);
 		return cTable;
 	}
 
 	private static CombineTable getTable2() {
-		String[][] datas = new String[10][6];
+		String[][] datas = new String[9][6];
 		for (int i = 0; i < datas.length; i++) {
 			String[] data = datas[i];
 			for (int j = 0; j < data.length; j++) {
