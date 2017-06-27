dc9ad6d42bc6fb14a0103af30752514d5fa3ea2e
WinChuan <huangchuan5@yonyou.com>
2017-06-27 15:27:19 +0800

    测试甘特图之前
    Change-Id: I0000000000000000000000000000000000000089
    Signed-off-by: WinChuan <huangchuan5@yonyou.com>

--- src/TableTest.java
+++ src/TableTest.java
@@ -15,6 +15,9 @@ import com.CombineData;
 import com.CombineTable;
 import com.cherish.header.DefaultGroup;
 import com.cherish.header.GroupableTableHeader;
+import javax.swing.JLabel;
+import javax.swing.JSplitPane;
+import javax.swing.JButton;
 
 public class TableTest {
 
@@ -52,49 +55,62 @@ public class TableTest {
 		frame.setBounds(100, 100, 1244, 716);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel);
-
-		JPanel panel2 = new JPanel();
+		JPanel panel1 = new JPanel();      //上部存放表格
+		frame.getContentPane().add(panel1);
+		panel1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel2 = new JPanel();     //下底部panel
 		frame.getContentPane().add(panel2);
+		panel2.setLayout(new GridLayout(1,2,0,0));
+		
+		
+		JPanel panel3 = new JPanel();    //下左
+		panel2.add(panel3);
+		JPanel panel4 = new JPanel();    //下右用来存放条形图
+		panel2.add(panel4);
+		
+		
 
-		String[][] cellData = {
+		String[][] cellData = {                                                          //table1数据
 				{"M00001", "P001", "红色", "1000", "2月5日", "还原工段", "1000", "0",
-						"1#", "10", "24", "240", "8", "8","8"},
+						           "1#", "10", "24", "240", "8", "8","8","7","8","9"},
 				{"M00001", "P001", "红色", "1000", "2月5日", "还原工段", "1000", "0",
-							"1#", "10", "24", "240", "8", "8","8"},
+							       "2#", "10", "24", "240", "8", "8","8","7","8","9"},
 				{"M00001", "P001", "红色", "1000", "2月5日", "还原工段", "1000", "0",
-								"1#", "10", "24", "240", "8", "8","8" },
+								   "3#", "10", "24", "240", "8", "8","8","7","8","9"},
 				{"M00002", "P002", "黄色", "1000", "2月6日", "还原工段", "2000", "2",
-						"1#", "10", "24", "240", "8", "8","8"},
+						           "1#", "10", "24", "240", "8", "8","8","7","8","9"},
 				{"M00002", "P002", "黄色", "1000", "2月6日", "还原工段", "2000", "2",
-							"1#", "10", "24", "240", "8", "8","8"},
+							       "2#", "10", "24", "240", "8", "8","8","7","8","9"},
 				{"M00002", "P002", "黄色", "1000", "2月6日", "还原工段", "2000", "2",
-								"1#", "10", "24", "240", "8", "8","8" },
+								   "3#", "10", "24", "240", "8", "8","8","7","8","9" },
 				{"M00003", "P003", "蓝色", "1000", "2月6日", "还原工段", "3000", "3",
-						"1#", "10", "24", "240", "8", "8","8"},
+						           "1#", "10", "24", "240", "8", "8","8","7","8","9"},
 				{"M00003", "P003", "蓝色", "1000", "2月6日", "还原工段", "3000", "3",
-							"1#", "10", "24", "240", "8", "8","8" },
+							       "2#", "10", "24", "240", "8", "8","8","7","8","9"},
 				{"M00003", "P003", "蓝色", "1000", "2月6日", "还原工段", "3000", "3",
-								"1#", "10", "24", "240", "8", "8","8" } };
+								   "3#", "10", "24", "240", "8", "8","8","7","8","9" },
+				{"M00004", "P003", "蓝色", "1000", "2月6日", "还原工段", "3000", "3",
+									"1#", "10", "24", "240", "8", "8","8","7","8","9"},
+				{"M00004", "P003", "蓝色", "1000", "2月6日", "还原工段", "3000", "3",
+									"2#", "10", "24", "240", "8", "8","8","7","8","9"},
+				{"M00004", "P003", "蓝色", "1000", "2月6日", "还原工段", "3000", "3",
+									"3#", "10", "24", "240", "8", "8","8","7","8","9"}
+		        };
 		String[] columnNames = { "生产订单", "产品", "规格", "计划数量", "完工日期", "工段",
 				"已排程数量", "未排程数量", "可用机台", "产品生产能力", "排程时间", "排程数量", "2月4号",
 				"2月5号" };
 		
 		String[] columnNames2 = { "生产订单", "产品", "规格", "计划数量", "完工日期", "工段",
 				"已排程数量", "未排程数量", "可用机台", "产品生产能力", "排程时间", "排程数量","6月5号","6月6号","6月7号"};
-		String[] columnNames22 = {"","","","","","","","","","","","","","",""};
-		panel.setLayout(new BorderLayout(0, 0));
+		String[] columnNames22 = {"","","","","","","","","","","","","","","","","",""};
 		
-		JTable table = new JTable(cellData, columnNames);
-		panel.add(new JScrollPane(table));
 
-
-		JTable table2 = this.getTable(cellData,columnNames22);                  //下表
+		JTable table1 = this.getTable(cellData,columnNames22);                  //下表
 		
-		table2.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		table1.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         GroupableTableHeader tableHeader = new GroupableTableHeader();
-        table2.setTableHeader(tableHeader);
+        table1.setTableHeader(tableHeader);
         
         
         for(int i = 0 ;i < 12 ; i++){
@@ -130,20 +146,48 @@ public class TableTest {
             group.setColumn(14);
             group.setHeaderValue("晚");
             tableHeader.addGroup(group);
+            
+            group = new DefaultGroup();
+            group.setColumn(15);                                     //6月6号
+            group.setColumnSpan(3);
+            group.setHeaderValue(columnNames2[13]);
+            tableHeader.addGroup(group);
+            
+            group = new DefaultGroup();
+            group.setRow(1);
+            group.setColumn(15);
+            group.setHeaderValue("早");
+            tableHeader.addGroup(group);
+
+            group = new DefaultGroup();
+            group.setRow(1);
+            group.setColumn(16);
+            group.setHeaderValue("中");
+            tableHeader.addGroup(group);
+            
+            group = new DefaultGroup();
+            group.setRow(1);
+            group.setColumn(17);
+            group.setHeaderValue("晚");
+            tableHeader.addGroup(group);
         
-       
-        
-//        initHeader(table2);
-        
-        
-		panel2.setLayout(new BorderLayout(0, 0));
-		panel2.add(new JScrollPane(table2), BorderLayout.CENTER);
-		table2.getTableHeader().setPreferredSize(new Dimension(1,42)); //设定表头高度
+	
+		panel1.add(new JScrollPane(table1), BorderLayout.CENTER);
+		table1.getTableHeader().setPreferredSize(new Dimension(1,42)); //设定表头高度
 
         
 	}
+	String[][] table2Datas = {
+			{"1#","","","","","",""},
+			{"1#","","","","","",""},
+			{"2#","","","","","",""},
+			{"2#","","","","","",""},
+			{"3#","","","","","",""},
+			{"3#","","","","","",""}
+	};
+//	JTable table2 = getTable();
 	
-	private static JTable getTable(String[][] datas,String[] columnNames) {                
+	private static JTable getTable(String[][] datas,String[] columnNames) {                 
 		ArrayList<Integer> combineColumns = new ArrayList<Integer>();   //用来保存需要合并的列数
 		for(int i = 0 ; i < 7 ; i++){
 			combineColumns.add(i);
